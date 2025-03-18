1819c99130da3ad8e4510e54ac12670eeac39ea0
Oliver Magnusson <118819332+Xurasta@users.noreply.github.com>
2025-03-18 09:45:17 +0100

    start

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-import "dotenv/config"
-import express from "express"
-import nunjucks from "nunjucks"
+import 'dotenv/config'
+import express from 'express'
+import nunjucks from 'nunjucks'
+import logger from 'morgan'
+import bodyParser from 'body-parser'
 
 const app = express()
 const port = 3000
@@ -10,12 +12,19 @@ nunjucks.configure("views", {
   express: app,
 })
 
+app.use(logger("dev"))
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-  res.render("index.njk",
-    { title: "Qvixter", message: "Best service, legit." }
-  )
+  res.render('index.njk', {
+    title: 'LoginTest',
+  })
+})
+
+app.get("/login", (req, res) => {
+  res.render('login.njk', {
+    title: 'LoginPage'
+  })
 })
 
 app.listen(port, () => {
